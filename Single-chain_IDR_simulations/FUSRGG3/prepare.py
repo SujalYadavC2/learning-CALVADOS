fd09637613ed9f93d577b14e66034fe6fdced3cf
Sujal <sujalyadav91ind@gmail.com>
2025-06-02 17:14:45 +0530

    simulation of FUSRGG3

---
+++ Single-chain_IDR_simulations/FUSRGG3/prepare.py
@@ -0,0 +1,35 @@
+import os
+from calvados.cfg import Config, Components
+
+cwd = os.getcwd()
+sysname = 'FUSRGG3'
+
+config = Config(
+    sysname = sysname,
+    box = [50,50,50], #nm
+    temp = 273,
+    pH = 7.5,
+    ionic = 0.19,
+
+    wfreq = 1000,
+    steps  = 1010*1000,   
+    restart = 'checkpoint',
+    frestart = 'restart.chk',
+    verbose = True,
+)
+
+path = f'{cwd}/{sysname}'
+
+config.write(path, 'config.yaml')
+
+components = Components(
+    molecule_type = 'protein',
+    nmol=1,
+    charge_termini='both',
+    fresidues = f'{cwd}/input/residues_CALVADOS2.csv',
+    ffasta = f'{cwd}/input/idr.fasta',
+)
+
+components.add(name='FUSRGG3')
+
+components.write(path,'components.yaml')
